cb78d80ef10384204f9f651b10074fd9d8768180
Enrico Corticelli <enrico.corticelli@gmail.com>
2024-05-02 12:28:30 +0200

    Aggiunge pagina artisti con mock + fix js

--- routes/router.js
+++ routes/router.js
@@ -2,17 +2,39 @@ const express = require("express");
 const router = express.Router();
 const { requiresAuth } = require('express-openid-connect');
 
-router.get('/', function (req, res, next) {
+router.get('/', requiresAuth(), function (req, res, next) {
   res.render('index', {
-    title: 'Auth0 Webapp sample Nodejs',
     isAuthenticated: req.oidc.isAuthenticated()
   });
 });
 
 router.get('/profile', requiresAuth(), function (req, res, next) {
+  console.log(JSON.stringify(req.oidc.user, null, 2))
+
   res.render('profile', {
     userProfile: JSON.stringify(req.oidc.user, null, 2),
     title: 'Profile page'
+  });
+});
+
+router.get('/artisti', requiresAuth(), function (req, res, next) {
+  const artistiJson =  [
+      {
+        nome: "Nome Artista 1",
+        descrizione: "Descrizione Artista 1",
+        dataCreazione: "2024-04-30",
+        dataUltimaModifica: "2024-05-01",
+      },
+      {
+        nome: "Nome Artista 2",
+        descrizione: "Descrizione Artista 2",
+        dataCreazione: "2024-04-25",
+        dataUltimaModifica: "2024-04-30",
+      },
+    ];
+
+  res.render('artisti', {
+    artisti: artistiJson
   });
 });
 
